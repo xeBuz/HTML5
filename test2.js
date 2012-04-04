6001bfb857cf571d9f74fc67a49ee14bb0119bdf
Jesus Roldan <xebuz18@gmail.com>
2012-04-04 01:01:10 -0300

    Pruebas

--- test2.js
+++ test2.js
@@ -6,13 +6,23 @@ function start(){
 	Crafty.scene("game", function(){
 		Crafty.background("gray");
 		
-		Crafty.e("2D, DOM, Color, Player, Multiway")
-			.color("red")
-			.multiway({x:2,y:1}, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
+		drawPlayer();
 
 	});
 	Crafty.scene("game");
 };
+
+
+function drawPlayer(){
+	Crafty.e("2D, DOM, Color, Player, Multiway")
+			.color("blue")
+			.multiway({x:2,y:1}, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
+};
+
+function drawEnemies(){
+	
+};
+
 
 Crafty.c("Player",{
 	_size : 10,
@@ -24,24 +34,26 @@ Crafty.c("Player",{
 				   y: this._posY, 
 				   w: this._size, 
 				   h: this._size})
-				   
-		this.requires("Multiway").multiway(1,{UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
-		/*
-		this.bind('KeyDown', function(e) {
-		if(e.key == Crafty.keys['LEFT_ARROW']) {
-			this.x=this.x-1;} 
-		else if (e.key == Crafty.keys['RIGHT_ARROW']) {
-			this.x=this.x+1;} 
-		else if (e.key == Crafty.keys['UP_ARROW']) {
-			this.y=this.y-1;} 
-		else if (e.key == Crafty.keys['DOWN_ARROW']) {
-			this.y=this.y+1;}
-		});
-		*/
+		this.requires("Color").color("blue");		   
+		//this.requires("Circle").circle(0, 0, 10);
+		//this.requires("Circle").dcircle.shift(5,5);
+		//{x: 5, y: 5, radius: 10};
+		//this.requires("Multiway").multiway(1,{UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180})
 	}
 });
 
 
-
-
-
+Crafty.c("RedEnemy",{
+	_size : 5,
+    _posX : Math.floor(Math.random()* 300),
+	_posY : Math.floor(Math.random()* 300),
+	
+	init: function(){
+		this.attr({x: this._posX, 
+				   y: this._posY, 
+				   w: this._size, 
+				   h: this._size})
+		this.requires("Color").color("red");
+	}
+	
+});
