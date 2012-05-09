d5d670e17831b29a74fe95898fece261447883d0
Jesus Roldan <xebuz18@gmail.com>
2012-05-09 17:39:18 -0300

    Added speed and jump to Player

--- Crafty/Its raining blocks/scripts/player.js
+++ Crafty/Its raining blocks/scripts/player.js
@@ -1,69 +1,65 @@
 Crafty.c("MainPlayer",{
 	_size : 25,
 	_life : 100,
+	_speed: 3,
+	_jump: 3.3,
 	init: function(){
-		  		   this.attr({x: 10, y: 10, z: 1, w: this.size, h: this.size})
-		  		   this.requires("Twoway, Gravity, Keyboard, PlayerSprite, SpriteAnimation, Collision")
-		  		   this.gravity("Floor")
-    	       	   this.twoway(3,3.3)
-    	       	   
-				   this.bind('Moved', function(from) {
-				   		if(this.hit('Block')) {
-				       		this.attr({x: from.x, y:from.y});
-				       	}
-					})
-					
-		  }
-		
-
+  		   this.attr({x: 10, y: 10, z: 1, w: this.size, h: this.size})
+  		   this.requires("Twoway, Gravity, Keyboard, PlayerSprite, SpriteAnimation, Collision")
+  		   this.gravity("Floor")
+  		   this.twoway(this.speed,this.jump)
+   
+		   this.bind('Moved', function(from) {
+		   		if(this.hit('Block')) {
+		       		this.attr({x: from.x, y:from.y});
+		       	}
+		   })
+	}
 });
 
 
 
 function createPlayer(){
-	player = Crafty.e("2D, DOM, MainPlayer")	
+	player = Crafty.e("2D, DOM, MainPlayer, Controls")	
 }
 
 
-
 // Ejemplo sacado de la web
-// Lo dejo aca pero no lo uso
-Crafty.c('CustomControls', {
-	__move: {left: false, right: false, up: false, down: false},    
+Crafty.c('Controls',{
+	_move:  {left: false, right: false, up: false, down: false}, 
 	_speed: 3,
+
+	Controls: function(speed){
+	   	if (speed) this._speed = speed;
+		var move = this._move;
+
+		this.bind('enterframe', function(){
+			if (move.right) this.x += this._speed; else 
+			if (move.left) this.x -= this._speed; else 
+			if (move.up) this.y -= this._speed; else 
+			if (move.down) this.y += this._speed; 		
+		}).bind('keydown', function(e) {
+	        	// Default movement booleans to false
+		    	move.right = move.left = move.down = move.up = false;
 	
-	CustomControls: function(speed) {
-	  if (speed) this._speed = speed;
-	  var move = this.__move;
+		        // If keys are down, set the direction
+        	        if (e.keyCode === Crafty.keys.RA) move.right = true;
+		        if (e.keyCode === Crafty.keys.LA) move.left = true;
+		        if (e.keyCode === Crafty.keys.UA) move.up = true;
+    		        if (e.keyCode === Crafty.keys.DA) move.down = true;
 	
-	  this.bind('enterframe', function() {
-	    // Move the player in a direction depending on the booleans
-	    // Only move the player in one direction at a time (up/down/left/right)
-	    if (move.right) this.x += this._speed; 
-	    else if (move.left) this.x -= this._speed; 
-	    else if (move.up) this.y -= this._speed;
-	    else if (move.down) this.y += this._speed;
-	  }).bind('keydown', function(e) {
-	    // Default movement booleans to false
-	    move.right = move.left = move.down = move.up = false;
+		    	this.preventTypeaheadFind(e);
+	  	}).bind('keyup', function(e) {
+		    	// If key is released, stop moving
+		        if (e.keyCode === Crafty.keys.RA) move.right = false;
+		        if (e.keyCode === Crafty.keys.LA) move.left = false;
+		        if (e.keyCode === Crafty.keys.UA) move.up = false;
+		        if (e.keyCode === Crafty.keys.DA) move.down = false;
 	
-	    // If keys are down, set the direction
-	    if (e.keyCode === Crafty.keys.RA) move.right = true;
-	    if (e.keyCode === Crafty.keys.LA) move.left = true;
-	    if (e.keyCode === Crafty.keys.UA) move.up = true;
-	    if (e.keyCode === Crafty.keys.DA) move.down = true;
+		        this.preventTypeaheadFind(e);
+	      	});
 	
-	    this.preventTypeaheadFind(e);
-	  }).bind('keyup', function(e) {
-	    // If key is released, stop moving
-	        if (e.keyCode === Crafty.keys.RA) move.right = false;
-	        if (e.keyCode === Crafty.keys.LA) move.left = false;
-	        if (e.keyCode === Crafty.keys.UA) move.up = false;
-	        if (e.keyCode === Crafty.keys.DA) move.down = false;
-	
-	        this.preventTypeaheadFind(e);
-	      });
-	
-	      return this;
-	    }
-});
+	      return this;	
+	}
+})
+
