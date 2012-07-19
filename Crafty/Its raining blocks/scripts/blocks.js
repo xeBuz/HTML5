bc1e87a39a826111e95b03d30ed11141621f84b6
Jesus Roldan <xebuz18@gmail.com>
2012-07-19 08:54:49 -0300

    Cleaning code

--- Crafty/Its raining blocks/scripts/blocks.js
+++ Crafty/Its raining blocks/scripts/blocks.js
@@ -12,7 +12,7 @@ function createBlocks(SPEED, TIME, SIZE){
 
 
 Crafty.c("Block",{ 
-	size    : 20,
+	size    : 20,    // From parameters
 	posFrom : 0, 
 	posTo   : WIDTH, // This is for a To Do. A movable scene.
 	div     : 0,
@@ -26,30 +26,7 @@ Crafty.c("Block",{
 		   		   h: this.size})  	   
 			this.color("black")
 
-			/*
-			this.onHit("Block", function(from){
-				this.attr({x: from.x, y:from.y})
-			});
-			*/
-  			
-  			/*
-  			this.bind('EnterFrame', function() {
-		    	if(this.hit('Moved')) {
-       				this.attr({x: from.x, y:from.y});
-       			}   
-  			});
-
-			*/
-		/*	this.bind('Moved', function(from) {
-				if(this.hit('Floor')){
-					this.requires("Gravity").gravity("Block")
-				}
-				if (this.hit('Block')){
-					this.requires("Gravity").gravity("Block")
-				}
-			});
-               */
-
+			// Second parameter MUST be a level-parameter
 			this.delay(function() {this.drop()}, 500);
 
 
